5083ac5fb34ff27e7a7b79fa67c79e95bbeb35ed
Solidx74 <kareebsadab@gmail.com>
2025-03-04 11:01:14 -0500

    runme.py(General Skills)

---
+++ runme.py
@@ -0,0 +1,20 @@
+1.Enter the terminal.
+2.Download the runme.py file.
+3.Then do what i did -
+┌──(kali㉿kali)-[~]
+└─$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos                                                                                                                                                                   
+┌──(kali㉿kali)-[~]
+└─$ cd Downloads                                                                                                                                                                       
+┌──(kali㉿kali)-[~/Downloads]
+└─$ ls
+runme.py                                                                                                                                                                  
+┌──(kali㉿kali)-[~/Downloads]
+└─$ cat runme.py   
+#!/usr/bin/python3
+################################################################################
+# Python script which just prints the flag
+################################################################################
+flag ='picoCTF{run_s4n1ty_run}'
+print(flag)
+4. Thus you get your flag pretty easily.Problem solved.
